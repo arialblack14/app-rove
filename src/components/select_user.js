4e9a51154ed80a54b2c12b3df9e77879ae44eeda
Alex Tsiras <tsirasalexis@yahoo.gr>
2017-05-26 19:35:06 +0300

    fetch all users successfully

--- src/components/select_user.js
+++ src/components/select_user.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchUsers } from '../actions/index'
 
 class SelectUser extends Component {
+  componentWillMount() {
+    this.props.fetchUsers()
+  }
+
   render() {
     return (
       <div>Hello from select user!</div>
@@ -8,4 +15,8 @@ class SelectUser extends Component {
   }
 }
 
-export default SelectUser
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchUsers }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SelectUser)
